e78c3ed49d1474bfe751d5d2922ff5e92ddf9fbc
EdisonR2024 <164270204+EdisonR2024@users.noreply.github.com>
2024-10-08 17:30:38 -0500

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const cabecera=document.querySelector('header');
+const menu=document.querySelector('nav>a');
+const tarjeta_imagen=document.querySelector('#imagenes>div');
+const pie_pagina=document.querySelector('footer');
+
+console.log(cabecera);
+console.log(menu);
+console.log(tarjeta_imagen);
+console.log(pie_pagina);
+
+console.log(document.title);
